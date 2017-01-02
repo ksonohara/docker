3e8d06fea0cf6064369c7e7a36aa70e5e084c262
K.Sonohara <sonohara@e-software.company>
2017-01-02 23:08:16 +0900

    remove exec nice

--- samba4-ad/service.sh
+++ samba4-ad/service.sh
@@ -6,4 +6,4 @@ sed -i "/dns forwarder = /d" /etc/samba/smb.conf
 sed -i "5a dns forwarder=$DNS_FORWARD" /etc/samba/smb.conf
 
 cat /etc/samba/smb.conf
-exec ionice /usr/sbin/samba -i -s /etc/samba/smb.conf
+/usr/sbin/samba -i -s /etc/samba/smb.conf
