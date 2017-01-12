3fdff1656da459b7ae296991d13d984588033bb7
K.Sonohara <sonohara@e-software.company>
2017-01-13 01:34:05 +0900

    fix exec samba

--- samba4-ad/service.sh
+++ samba4-ad/service.sh
@@ -6,5 +6,4 @@ sed -i "/dns forwarder = /d" /etc/samba/smb.conf
 sed -i "5a dns forwarder=$DNS_FORWARD" /etc/samba/smb.conf
 
 cat /etc/samba/smb.conf
-/usr/sbin/samba -i -s /etc/samba/smb.conf
 exec ionice -c 3 /usr/sbin/samba -i -s /etc/samba/smb.conf < /dev/null
